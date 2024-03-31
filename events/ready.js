26afc36fd1512dff04f128fe376f367a6c346e21
Donald J. Fortier II <Technical_13@yahoo.com>
2024-03-31 09:09:21 -0400

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,21 @@
+const Discord = require( 'discord.js' );
+const mongoose = require( 'mongoose' );
+
+module.exports = {
+	name: 'ready',
+	once: true,
+	async run( client ) {
+    
+    mongoose.set( 'strictQuery', false );
+    client.user.setActivity( 'Dungeons & Dragons Online', { type: 'PLAYING' } );
+    mongoose.disconnect( () => console.log( 'Closed MongoDBs.' ) );
+    await mongoose.connect( process.env.mongodb || '', { keepAlive: true } )
+      .then( connected => { console.log( 'Connected to MongoDB.' ); } )
+      .catch( errDB => {
+        console.error( 'Failed to connect to MongoDB:\n\t%s\n\t%o',
+          Array.from( errDB.reason.servers.keys() ).join( '\n\t' ), errDB.message );
+      } );
+    
+    console.log( 'Successfully logged in as: ' + client.user.tag );
+	}
+}
