90acddaa53c446bddf1a0e80b5622bfbbaa37bbe
Donald J. Fortier II <Technical_13@yahoo.com>
2024-11-12 13:18:36 -0500

    Consistency pass for /commands/

--- commands/admin/roadmap.js
+++ commands/admin/roadmap.js
@@ -1,4 +1,6 @@
+const chalk = require( 'chalk' );
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder } = require( 'discord.js' );
+const strScript = chalk.hex( '#FFA500' ).bold( './commands/admin/roadmap.js' );
 
 module.exports = {
   name: 'roadmap',
@@ -6,30 +8,32 @@ module.exports = {
   description: 'Development ToDo list for me!',
   cooldown: 600000,
   run: async ( client, message, args ) => {
-    const arrToDo = [
-      'Move `§part` & `§inspectdb` to `/system`',
-      'Update `/config` and `/system` for DB changes',
-      'Do whatever is defined for guildConfig.Part in guildMemberKick.js, guildMemberBan.js, and/or guildMemberDelete.js',
-      'Finish guildCreate.js event telling guild owner how to config bot.',
-      'Get the `/bot` command working again so people can get bot info and links to request features and report bugs.',
-      'Move this list so I can add/modify/remove items from Discord.' +
-      '\nAdd integration to make this list an issue on GitHub and be able to view/edit all issues?' +
-      '\n:arrow_right: <https://docs.github.com/en/rest/quickstart?apiVersion=latest>' +
-      '\n:arrow_right: <https://docs.github.com/en/rest/guides/scripting-with-the-rest-api-and-javascript?apiVersion=latest>' +
-      '\n:arrow_right: <https://docs.github.com/en/rest/issues/issues?apiVersion=latest>',
-      'Get `/guilds` and `/cipher` working.',
-      'Create `/verify` command.',
-      'Make `/ban` command to store better information on bans.'
-    ];
-    const embedToDo = new EmbedBuilder()
-      .setTitle( 'Development Roadmap for bot:' )
-      .setColor( '#FF00FF' )
-      .setTimestamp()
-      .setFooter( { text: client.user.tag } );
+    try {
+      const arrToDo = [
+        'Move `§help` to `/help` so response can be ephemeral',
+        'Move `§invite` to `/invite` and make User cmd so response can be ephemeral',
+        'Update `/config` and `/system` for DB changes',
+        'Do whatever is defined for guildConfig.Part in guildMemberKick.js and guildMemberBan.js',
+        'Finish guildCreate.js event telling guild owner how to config bot.',
+        'Get the `/bot` command working again and make User cmd, so people can get bot info and links to request features and report bugs.',
+        'Move this list so I can add/modify/remove items from Discord.' +
+        '\nAdd integration to make this list an issue on GitHub and be able to view/edit all issues?' +
+        '\n:arrow_right: <https://docs.github.com/en/rest/quickstart?apiVersion=latest>' +
+        '\n:arrow_right: <https://docs.github.com/en/rest/guides/scripting-with-the-rest-api-and-javascript?apiVersion=latest>' +
+        '\n:arrow_right: <https://docs.github.com/en/rest/issues/issues?apiVersion=latest>',
+        'Get `/guilds` and `/cipher` working.',
+        'Create `/verify` command.',
+        'Store information on TOs, kicks, and bans.'
+      ];
+      const embedToDo = new EmbedBuilder()
+        .setTitle( 'Development Roadmap for bot:' )
+        .setColor( '#FF00FF' )
+        .setTimestamp()
+        .setFooter( { text: client.user.tag } );
 
-    arrToDo.forEach( item => {
-        embedToDo.addFields( { name: '\u200B', value: item, inline: false } );
-    } );
-    message.reply( { embeds: [ embedToDo ] } );
+      arrToDo.forEach( item => { embedToDo.addFields( { name: '\u200B', value: item, inline: false } ); } );
+      message.reply( { embeds: [ embedToDo ] } );
+    }
+    catch ( errObject ) { console.error( 'Uncaught error in %s:\n\t%s', strScript, errObject.stack ); }
   }
 };
