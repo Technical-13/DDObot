a5a66fe00d0552c8ce2e5a70ad5cd2badccddfe7
Donald J. Fortier II <Technical_13@yahoo.com>
2025-06-07 20:44:09 -0400

    Done trying to match regex...
    Instead of trying to make it fit a regex pattern, just trash anything after a pipe then split on colon. Last section is page, first section is site (if not default).

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -56,8 +56,8 @@ client.on( 'messageCreate', async ( message ) => {
     const arrTemplateLinks = ( noCodeContent.match( regTemplateLinks ) || [] );
     if ( arrTemplateLinks.length >= 1 ) {
       for ( let rawLink of arrTemplateLinks ) {console.log('rawLink: %o',rawLink);
-        const arrRawLink = rawLink.match( /\{\{((.*?)\:)?(.*?)(\|(.*?))?/ );console.log('arrRawLink: %o',arrRawLink);
-        const cleanLink = arrRawLink[ 1 ];console.log('cleanLink: %o',cleanLink);
+        const arrRawLink = rawLink.replace( /[\{\}]/g, '' ).split( '|' )[ 0 ].split( ':' );console.log('arrRawLink: %o',arrRawLink);
+        const cleanLink = arrRawLink.pop();console.log('cleanLink: %o',cleanLink);
         const cleanSite = ( !arrRawLink[ 0 ] || arrRawLink[ 0 ]?.toLowerCase() === 't:' || arrRawLink[ 0 ]?.toLowerCase() === 'template:' ? '' : arrRawLink[ 0 ] );console.log('cleanSite: %o',cleanSite);
         foundLinks.push( '[' + cleanSite + 'Template:' + cleanLink + ']' );
       }
