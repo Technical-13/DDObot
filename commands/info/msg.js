0c0d90058972e5ba93038f61fba45571c7583599
Donald J. Fortier II <Technical_13@yahoo.com>
2025-06-24 19:55:49 -0400

    Fix error handling and logging in msg command
    Corrected checks for channel and message existence, improved logging to handle error objects, and fixed a copy-paste error where the wrong variable was checked after fetching the channel and message.

--- commands/info/msg.js
+++ commands/info/msg.js
@@ -14,13 +14,13 @@ module.exports = {
       if ( args.length === 1 ) {
         const path = ( args[ 0 ].match( /https?:\/\/(?:ptb\.)?discord\.com\/channels\/(?<srvID>\d{18,20})\/(?<chanID>\d{18,20})\/(?<msgID>\d{18,20})/i ).groups || null );
         if ( !path ) { throw new Error( 'Unable to find message with link: ' + args[ 0 ] ); }
-        const guild = await client.guilds.fetch( path.srvID ).catch( errGuild => { return errGuild } );console.log( 'guild : %o', guild.toJSON() );
+        const guild = await client.guilds.fetch( path.srvID ).catch( errGuild => { return errGuild } );console.log( 'guild : %o', ( guild.id ? guild.toJSON() : guild ) );
         if ( !guild ) { throw new Error( 'I\'m not in a guild with an ID of: ' + path.srvID ); }
-        const channel = await guild.channels.fetch( path.chanID ).catch( errChan => { return errChan } );console.log( 'channel : %o', channel.toJSON() );
-        if ( !guild ) { throw new Error( 'I couldn\'t find a channel in the guild with an ID of: ' + path.chanID ); }
+        const channel = await guild.channels.fetch( path.chanID ).catch( errChan => { return errChan } );console.log( 'channel : %o', ( channel.id ? channel.toJSON() : channel ) );
+        if ( !channel ) { throw new Error( 'I couldn\'t find a channel in the guild with an ID of: ' + path.chanID ); }
         const message = await channel?.messages.fetch( path.msgID ).catch( errMsg => { return errMsg } );
-        if ( !guild ) { throw new Error( 'I couldn\'t find a message in the channel with an ID of: ' + path.msgID ); }
-        console.log( 'message: %o', message.toJSON() );
+        if ( !message ) { throw new Error( 'I couldn\'t find a message in the channel with an ID of: ' + path.msgID ); }
+        console.log( 'message: %o', ( message.id ? message.toJSON() : message ) );
       }
       else { console.log( 'Command was malformed.  Please try again.' ); }
 
