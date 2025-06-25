e5bc135c7de1adfea1ea921ce92eb01091d6bc29
Donald J. Fortier II <Technical_13@yahoo.com>
2025-06-24 20:03:09 -0400

    Improve error handling for message link parsing
    Updated the checks for guild, channel, and message fetching to verify the presence of the 'id' property and provide more informative error messages. This enhances debugging and user feedback when invalid IDs or fetch errors occur.

--- commands/info/msg.js
+++ commands/info/msg.js
@@ -14,12 +14,12 @@ module.exports = {
       if ( args.length === 1 ) {
         const path = ( args[ 0 ].match( /https?:\/\/(?:ptb\.)?discord\.com\/channels\/(?<srvID>\d{18,20})\/(?<chanID>\d{18,20})\/(?<msgID>\d{18,20})/i ).groups || null );
         if ( !path ) { throw new Error( 'Unable to find message with link: ' + args[ 0 ] ); }
-        const guild = await client.guilds.fetch( path.srvID ).catch( errGuild => { return errGuild } );console.log( 'guild : %o', ( guild.id ? guild.toJSON() : guild ) );
-        if ( !guild ) { throw new Error( 'I\'m not in a guild with an ID of: ' + path.srvID ); }
-        const channel = await guild.channels.fetch( path.chanID ).catch( errChan => { return errChan } );console.log( 'channel : %o', ( channel.id ? channel.toJSON() : channel ) );
-        if ( !channel ) { throw new Error( 'I couldn\'t find a channel in the guild with an ID of: ' + path.chanID ); }
-        const message = await channel?.messages.fetch( path.msgID ).catch( errMsg => { return errMsg } );
-        if ( !message ) { throw new Error( 'I couldn\'t find a message in the channel with an ID of: ' + path.msgID ); }
+        const guild = await client.guilds.fetch( path.srvID ).catch( errGuild => { return errGuild } );
+        if ( !guild.id ) { throw new Error( guild.message + ' for guild with an ID of: ' + path.srvID ); }
+        const channel = await guild.channels.fetch( path.chanID ).catch( errChan => { return errChan } );
+        if ( !channel.id ) { throw new Error( channel.message + ' for channel with an ID of: ' + path.chanID ); }
+        const message = await channel.messages.fetch( path.msgID ).catch( errMsg => { return errMsg } );
+        if ( !message.id ) { throw new Error( message.message + ' for message with an ID of: ' + path.msgID ); }
         console.log( 'message: %o', ( message.id ? message.toJSON() : message ) );
       }
       else { console.log( 'Command was malformed.  Please try again.' ); }
