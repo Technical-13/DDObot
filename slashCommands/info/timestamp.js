05ca5a44d1530885d0e931f7ff96d7efe6e755d1
Donald J. Fortier II <Technical_13@yahoo.com>
2025-07-17 19:55:51 -0400

    Add timestamp slash command for Discord
    Introduces a new /timestamp command that generates dynamic Discord timestamps based on user input. The command validates input using the DDOwiki API and provides various timestamp formats for use in Discord messages.

---
+++ slashCommands/info/timestamp.js
@@ -0,0 +1,67 @@
+const config = require( '../../config.json' );
+const ddoWikiApiEndpoint = 'https://ddowiki.com/api.php?origin=*';
+const chalk = require( 'chalk' );
+const { ApplicationCommandType, InteractionContextType } = require( 'discord.js' );
+const getGuildConfig = require( '../../functions/getGuildDB.js' );
+const userPerms = require( '../../functions/getPerms.js' );
+const strScript = chalk.hex( '#FFA500' ).bold( './slashCommands/info/timestamp.js' );
+
+const validateInput = async function ( input ) {
+  var validateInputURL = ddoWikiApiEndpoint;
+  const params = {
+    action: 'parse',
+    format: 'json',
+    text: '{'+'{#time:U|' + input + '}'+'}',
+    disablelimitreport: 1,
+    contentmodel: 'wikitext',
+    formatversion: '2'
+  };
+  Object.keys( params ).forEach( key => { validateInputURL += '&' + encodeURIComponent( key ) + '=' + encodeURIComponent( params[ key ] ); } );
+  return fetch( validateInputURL ).then( resParsedData => { return resParsedData.json(); } ).then( data => {
+    if ( data.parse.text.includes( 'Invalid time' ) ) {
+      return 'INVALID';
+    }
+    else {
+      return data.parse.text.match( /<p>([\d]*)\n<\/p>/ )[ 1 ];
+    }
+  } ).catch( parseErr => {
+    console.log( 'Error attempting to validateInput( \'%s\' ) with params: %o\nReturned: %o ', input, params, parseErr );
+    return 'ERROR';
+  } );
+}
+
+module.exports = {
+  name: 'timestamp',
+  description: 'Get dynamic timestamps for Discord.',
+  type: ApplicationCommandType.ChatInput,
+  contexts: [ InteractionContextType.Guild ],
+  group: 'info',
+  devOnly: true,
+  options: [
+    { type: 3, name: 'datetime', description: 'When do you want a timestamp for?', required: true }
+  ],
+  cooldown: 1000,
+  run: async ( client, interaction ) => {
+    await interaction.deferReply( { ephemeral: true } );
+    const { channel, guild, options, user: member } = interaction;
+    const { isGlobalBlacklisted, content } = await userPerms( member, guild );
+
+    try {
+      const input = options.getString( 'datetime', true );
+      const timestamp = await validateInput( input );
+      if ( timestamp === 'INVALID' ) { return interaction.editReply( { content: 'I am unable to parse a date or time from `' + input + '`.  Please try again.' } ); }
+      if ( timestamp === 'ERROR' ) { return interaction.editReply( { content: 'I encountered an error attempting to parse a date or time from `' + input + '`.  Please try again later.' } ); }
+      return interaction.editReply( { content:
+        'Your timestamps for `' + input + '` are as follows:\n' +
+        '`<t:' + input + ':t>` :arrow_right: <t:' + input + ':t>' +
+        '`<t:' + input + ':T>` :arrow_right: <t:' + input + ':T>' +
+        '`<t:' + input + ':d>` :arrow_right: <t:' + input + ':d>' +
+        '`<t:' + input + ':D>` :arrow_right: <t:' + input + ':D>' +
+        '`<t:' + input + ':f>` :arrow_right: <t:' + input + ':f>' +
+        '`<t:' + input + ':F>` :arrow_right: <t:' + input + ':F>' +
+        '`<t:' + input + ':R>` :arrow_right: <t:' + input + ':R>'
+      } );
+    }
+    catch ( errObject ) { console.error( 'Uncaught error in %s:\n\t%s', strScript, errObject.stack ); }
+  }
+};
