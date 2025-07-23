7093387255c6cf8d7edcccf0210b37e6443dfa63
Donald J. Fortier II <Technical_13@yahoo.com>
2025-07-23 18:09:26 -0400

    Add timezone offset option to timestamp command
    Introduces an 'offset' option to the timestamp slash command, allowing users to specify their timezone offset. The offset defaults to Eastern Time and adjusts for Daylight Saving Time automatically.

--- slashCommands/info/timestamp.js
+++ slashCommands/info/timestamp.js
@@ -6,12 +6,13 @@ const getGuildConfig = require( '../../functions/getGuildDB.js' );
 const userPerms = require( '../../functions/getPerms.js' );
 const strScript = chalk.hex( '#FFA500' ).bold( './slashCommands/info/timestamp.js' );
 
-const validateInput = async function ( input ) {
+const isDst = new Date().getHours() !== new Date( new Date().getFullYear(), 0, 1 ).toLocaleString( 'en-US', { timeZone: 'America/New_York' } ).split( ', ' )[ 1 ].split( ':' )[ 0 ];
+const validateInput = async function ( input, offset = 4 + ( isDst ? 1 : 0 ) ) {
   var validateInputURL = wmfWikiEndpoint;
   const params = {
     action: 'parse',
     format: 'json',
-    text: '{'+'{#time:U|' + input + '}'+'}',
+    text: '{'+'{#time:U|' + input + ( offset === 0 ? '' : ( offset < 0 ? ' ' : ' +' ) + offset + ' hours' ) + '}'+'}',
     disablelimitreport: 1,
     contentmodel: 'wikitext',
     formatversion: '2'
@@ -38,6 +39,9 @@ module.exports = {
   group: 'info',
   options: [
     { type: 3, name: 'datetime', description: 'When do you want a timestamp for? (default `now`)' },
+    { type: 10, name: 'offset', min_value: -12, max_value: 14,
+      description: 'The offset for your timezone. (EG +' + ( isDst ? '4' : '5' ) + ' for Eastern ' + ( isDst ? 'Daylight' : 'Standard' ) + ' Time'
+    },
     { type: 3, name: 'format', description: 'Full output or a single format (for easier copy/paste)', choices: [
       { name: 'Full output with all options', value: 'a' },
       { name: 'Relative value such as `in 3 days`', value: 'R' },
