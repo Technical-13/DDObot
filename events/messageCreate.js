1ee31c2c8fa5b89222fa13a3df13fda1a261a9d2
Donald J. Fortier II <Technical_13@yahoo.com>
2026-08-25 12:09:40 -0400

    Enhance error logging in getSiteMatrix function

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -103,7 +103,7 @@ const getSiteMatrix = async function () {
       }
       else {
         const isJSON = !( 'is not valid JSON'.test( smErr.message ) );
-        console.error( 'Error attempting to getSiteMatrix() with smParams: %o\nReturned: %o ', smParams, isJSON ? smErr : smErr.stack );
+        console.error( 'Error attempting to getSiteMatrix() with smParams: %o\nFrom URL: %o\nReturned: %o ', smParams, fetchSiteMatrixURL, isJSON ? smErr : smErr.stack );
         smCustom.custom.map( proj => {
           if ( !smParams.smArray.find( objProj => objProj.lang == proj.lang ) ) {
             const siteCustom = { lang: proj.lang };
